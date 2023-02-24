eb52acf5565c6d7ae8d9430d2d89756965320200
Liam Eckert <liameckert17@gmail.com>
2023-02-24 14:57:44 -0500

    new gitignore switching to python

--- info.rkt
+++
@@ -1,9 +0,0 @@
-#lang info
-(define collection "spice")
-(define deps '("base"))
-(define build-deps '("scribble-lib" "racket-doc" "rackunit-lib"))
-(define scribblings '(("scribblings/spice.scrbl" ())))
-(define pkg-desc "A scheme dialect for the future of the web.")
-(define version "0.1")
-(define pkg-authors '(liameckert))
-(define license '(MIT))
