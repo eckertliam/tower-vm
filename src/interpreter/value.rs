fa32196b79e8cc9d77d5c6aa3d4c5dc5178cefc3
Liam Eckert <liameckert17@gmail.com>
2023-07-03 10:52:50 -0400

    Lossless conversions for values

--- src/interpreter/value.rs
+++ src/interpreter/value.rs
@@ -1,5 +1,8 @@
+use std::mem::transmute;
+
 #[derive(Debug, PartialEq, Clone, Copy)]
 pub enum Value {
+    I8(i8),
     I16(i16),
     I32(i32),
     I64(i64),
@@ -14,6 +17,10 @@ pub enum Value {
 }
 
 impl Value {
+    pub fn to_i8(raw: u64) -> Value {
+        Value::I8(raw as i8)
+    }
+
     pub fn to_i16(raw: u64) -> Value {
         Value::I16(raw as i16)
     }
@@ -27,11 +34,16 @@ impl Value {
     }
 
     pub fn to_f32(raw: u64) -> Value {
-        Value::F32(raw as f32)
+        unsafe {
+            Value::F32(transmute::<u32, f32>(raw as u32))
+        
+        }
     }
 
     pub fn to_f64(raw: u64) -> Value {
-        Value::F64(raw as f64)
+        unsafe {
+            Value::F64(transmute::<u64, f64>(raw))
+        }
     }
 
     pub fn to_bool(raw: u64) -> Value {
@@ -62,11 +74,20 @@ impl Value {
 impl Into<u64> for Value {
     fn into(self) -> u64 {
         match self {
+            Value::I8(v) => v as u64,
             Value::I16(v) => v as u64,
             Value::I32(v) => v as u64,
             Value::I64(v) => v as u64,
-            Value::F32(v) => v as u64,
-            Value::F64(v) => v as u64,
+            Value::F32(v) => {
+                unsafe {
+                    transmute::<f32, u32>(v) as u64
+                }
+            }
+            Value::F64(v) => {
+                unsafe {
+                    transmute::<f64, u64>(v)
+                }
+            }
             Value::Bool(v) => v as u64,
             Value::Char(v) => v as u64,
             Value::U8(v) => v as u64,
@@ -77,29 +98,198 @@ impl Into<u64> for Value {
     }
 }
 
+macro_rules! impl_try_into {
+    ($type:ty, $variant:ident) => {
+        impl TryInto<$type> for Value {
+            type Error = String;
+
+            fn try_into(self) -> Result<$type, Self::Error> {
+                match self {
+                    Value::$variant(v) => Ok(v),
+                    _ => Err(format!("Cannot convert {:?} to {}", self, stringify!($type))),
+                }
+            }
+        }
+    };
+}
+
+impl_try_into!(i8, I8);
+impl_try_into!(i16, I16);
+impl_try_into!(i32, I32);
+impl_try_into!(i64, I64);
+impl_try_into!(f32, F32);
+impl_try_into!(f64, F64);
+impl_try_into!(bool, Bool);
+impl_try_into!(char, Char);
+impl_try_into!(u8, U8);
+impl_try_into!(u16, U16);
+impl_try_into!(u32, U32);
+
+macro_rules! impl_from {
+    ($type:ty, $variant:ident) => {
+        impl From<$type> for Value {
+
+            fn from(value: $type) -> Self {
+                Value::$variant(value)
+            }
+        }
+    };
+}
+
+impl_from!(i8, I8);
+impl_from!(i16, I16);
+impl_from!(i32, I32);
+impl_from!(i64, I64);
+impl_from!(f32, F32);
+impl_from!(f64, F64);
+impl_from!(bool, Bool);
+impl_from!(char, Char);
+impl_from!(u8, U8);
+impl_from!(u16, U16);
+impl_from!(u32, U32);
+impl_from!(u64, U64);
+
+
+
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
+    // each of these round tests takes a value through the following steps:
+    // 1. create a Value from the initial value
+    // 2. convert the Value into a u64
+    // 3. convert the u64 back into a Value
+    // 4. convert the Value into the initial value type
+    // 5. assert that the initial value and the final value are equal
+    // this verifies lossless round-tripping through the lifecycle of a Value
     #[test]
-    fn test_to_i16() {
-        let n: u64 = 100;
-        let v = Value::to_i16(n);
-        assert_eq!(v, Value::I16(100));
+    fn test_i8_round() {
+        let init: i8 = 22;
+        let in_val: Value = init.into();
+        let encoded: u64 = in_val.into();
+        let decoded: Value = Value::to_i8(encoded);
+        let out: i8 = decoded.try_into().unwrap();
+        assert_eq!(init, out);
     }
 
     #[test]
-    fn test_to_i32() {
-        let n: u64 = 105;
-        let v = Value::to_i32(n);
-        assert_eq!(v, Value::I32(105));
+    fn test_i16_round() {
+        let init: i16 = 420;
+        let in_val: Value = init.into();
+        let encoded: u64 = in_val.into();
+        let decoded: Value = Value::to_i16(encoded);
+        let out: i16 = decoded.try_into().unwrap();
+        assert_eq!(init, out);
     }
 
     #[test]
-    fn test_to_i64() {
-        let n: u64 = 110;
-        let v = Value::to_i64(n);
-        assert_eq!(v, Value::I64(110));
+    fn test_i32_round() {
+        let init: i32 = 20000;
+        let in_val: Value = init.into();
+        let encoded: u64 = in_val.into();
+        let decoded: Value = Value::to_i32(encoded);
+        let out: i32 = decoded.try_into().unwrap();
+        assert_eq!(init, out);
+    }
+
+    #[test]
+    fn test_i64_round() {
+        let init: i64 = 2020100000000000;
+        let in_val: Value = init.into();
+        let encoded: u64 = in_val.into();
+        let decoded: Value = Value::to_i64(encoded);
+        let out: i64 = decoded.try_into().unwrap();
+        assert_eq!(init, out);
+    }
+
+    #[test]
+    fn test_f32_round() {
+        let init: f32 = std::f32::consts::PI;
+        let in_val: Value = init.into();
+        let encoded: u64 = in_val.into();
+        let decoded: Value = Value::to_f32(encoded);
+        let out: f32 = decoded.try_into().unwrap();
+        assert_eq!(init, out);
+    }
+
+    #[test]
+    fn test_f64_round() {
+        let init: f64 = std::f64::consts::PI;
+        let in_val: Value = init.into();
+        let encoded: u64 = in_val.into();
+        let decoded: Value = Value::to_f64(encoded);
+        let out: f64 = decoded.try_into().unwrap();
+        assert_eq!(init, out);
+    }
+
+    #[test]
+    fn test_true_round() {
+        let init: bool = true;
+        let in_val: Value = init.into();
+        let encoded: u64 = in_val.into();
+        let decoded: Value = Value::to_bool(encoded);
+        let out: bool = decoded.try_into().unwrap();
+        assert_eq!(init, out);
+    }
+
+    #[test]
+    fn test_false_round() {
+        let init: bool = false;
+        let in_val: Value = init.into();
+        let encoded: u64 = in_val.into();
+        let decoded: Value = Value::to_bool(encoded);
+        let out: bool = decoded.try_into().unwrap();
+        assert_eq!(init, out);
+    }
+
+    #[test]
+    fn test_char_round() {
+        let init: char = 'a';
+        let in_val: Value = init.into();
+        let encoded: u64 = in_val.into();
+        let decoded: Value = Value::to_char(encoded);
+        let out: char = decoded.try_into().unwrap();
+        assert_eq!(init, out);
+    }
+
+    #[test]
+    fn test_u8_round() {
+        let init: u8 = 22;
+        let in_val: Value = init.into();
+        let encoded: u64 = in_val.into();
+        let decoded: Value = Value::to_u8(encoded);
+        let out: u8 = decoded.try_into().unwrap();
+        assert_eq!(init, out);
+    }
+
+    #[test]
+    fn test_u16_round() {
+        let init: u16 = 420;
+        let in_val: Value = init.into();
+        let encoded: u64 = in_val.into();
+        let decoded: Value = Value::to_u16(encoded);
+        let out: u16 = decoded.try_into().unwrap();
+        assert_eq!(init, out);
+    }
+
+    #[test]
+    fn test_u32_round() {
+        let init: u32 = 20000;
+        let in_val: Value = init.into();
+        let encoded: u64 = in_val.into();
+        let decoded: Value = Value::to_u32(encoded);
+        let out: u32 = decoded.try_into().unwrap();
+        assert_eq!(init, out);
+    }
+
+    #[test]
+    fn test_u64_round() {
+        let init: u64 = 2020100000000000;
+        let in_val: Value = init.into();
+        let encoded: u64 = in_val.into();
+        let decoded: Value = Value::to_u64(encoded);
+        let out: u64 = decoded.try_into().unwrap();
+        assert_eq!(init, out);
     }
 }
