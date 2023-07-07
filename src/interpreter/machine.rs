af870da2dc2e4da4a67c8ff05fb995caffebe51a
Liam Eckert <liameckert17@gmail.com>
2023-07-06 20:52:07 -0400

    full instruction set implemented

--- src/interpreter/machine.rs
+++ src/interpreter/machine.rs
@@ -3,6 +3,29 @@ use super::value::{TypeFlag, Value};
 const STACK_SIZE: usize = 1024;
 
 
+macro_rules! binop_helper {
+    ($fn_name:ident, $op:tt) => {
+        fn $fn_name(&mut self) {
+            let lhs = self.value_pop();
+            let rhs = self.value_pop();
+            self.value_push(lhs $op rhs);
+        }
+    };
+}
+
+macro_rules! comp_helper {
+    ($fn_name:ident, $op:tt) => {
+        fn $fn_name(&mut self) {
+            let lhs = self.value_pop();
+            let rhs = self.value_pop();
+            let curr_ty = self.ty_flag;
+            self.ty_flag = TypeFlag::Bool;
+            self.value_push((lhs $op rhs).into());
+            self.ty_flag = curr_ty;
+        }
+    };
+}
+
 pub struct Machine {
     stack: [u64; STACK_SIZE],
     sp: usize,
@@ -62,35 +85,15 @@ impl Machine {
         self.stack_push(self.ty_flag.into());            
     }
 
-    fn add(&mut self) {
-        let lhs = self.value_pop();
-        let rhs = self.value_pop();
-        self.value_push(lhs + rhs);
-    }
+    binop_helper!(add, +);
 
-    fn sub(&mut self) {
-        let lhs = self.value_pop();
-        let rhs = self.value_pop();
-        self.value_push(lhs - rhs);
-    }
+    binop_helper!(sub, -);
 
-    fn mul(&mut self) {
-        let lhs = self.value_pop();
-        let rhs = self.value_pop();
-        self.value_push(lhs * rhs);
-    }
+    binop_helper!(mul, *);
 
-    fn div(&mut self) {
-        let lhs = self.value_pop();
-        let rhs = self.value_pop();
-        self.value_push(lhs / rhs);
-    }
+    binop_helper!(div, /);
 
-    fn rem(&mut self) {
-        let lhs = self.value_pop();
-        let rhs = self.value_pop();
-        self.value_push(lhs % rhs);
-    }
+    binop_helper!(rem, %);
 
     fn neg(&mut self) {
         self.stack[self.sp - 1] = (-(Value::from_stack(self.ty_flag, self.stack[self.sp - 1]))).to_stack();
@@ -106,30 +109,104 @@ impl Machine {
         self.stack[self.sp - 1] = (value - Value::from_stack(self.ty_flag, 1)).to_stack();
     }
 
-    fn eq(&mut self) {
-        let lhs = self.value_pop();
-        let rhs = self.value_pop();
-        let curr_ty = self.ty_flag;
-        self.ty_flag = TypeFlag::Bool;
-        self.value_push((lhs == rhs).into());
-        self.ty_flag = curr_ty;
+    comp_helper!(eq, ==);
+    comp_helper!(neq, !=);
+    comp_helper!(lt, <);
+    comp_helper!(gt, >);
+    comp_helper!(lte, <=);
+    comp_helper!(gte, >=);
+
+    binop_helper!(and, &);
+    binop_helper!(or, |);
+    binop_helper!(xor, ^);
+    binop_helper!(shl, <<);
+    binop_helper!(shr, >>);
+
+    fn not(&mut self) {
+        self.stack[self.sp - 1] = (!Value::from_stack(self.ty_flag, self.stack[self.sp - 1])).to_stack();
     }
 
-    fn neq(&mut self) {
-        let lhs = self.value_pop();
-        let rhs = self.value_pop();
-        let curr_ty = self.ty_flag;
-        self.ty_flag = TypeFlag::Bool;
-        self.value_push((lhs != rhs).into());
-        self.ty_flag = curr_ty;
+    fn jmp(&mut self) {
+        let addr = self.stack_pop() as usize;
+        self.ip = addr;
     }
 
-    fn lt(&mut self) {
-        let lhs = self.value_pop();
-        let rhs = self.value_pop();
-        let curr_ty = self.ty_flag;
-        self.ty_flag = TypeFlag::Bool;
-        self.value_push((lhs < rhs).into());
-        self.ty_flag = curr_ty;
+    fn jmp_if(&mut self) {
+        let cond = self.value_pop();
+        let addr = self.stack_pop() as usize;
+
+        if cond.to_stack() != 0 {
+            self.ip = addr;
+        }
     }
+
+    fn jmp_if_not(&mut self) {
+        let cond = self.value_pop();
+        let addr = self.stack_pop() as usize;
+
+        if cond.to_stack() == 0 {
+            self.ip = addr;
+        }
+    }
+
+    fn call(&mut self) {
+        let addr = self.stack_pop() as usize;
+        self.stack_push(self.ip as u64);
+        self.ip = addr;
+    }
+
+    fn ret(&mut self) {
+        self.ip = self.stack_pop() as usize;
+    }
+
+    fn push(&mut self) {
+        self.ip += 1;
+        let value = Value::from_code(self.ty_flag, &self.code[self.ip..self.ip + self.ty_flag.size()]);
+        self.value_push(value);
+        self.ip += self.ty_flag.size() - 1;
+    }
+
+    fn dup(&mut self) {
+        self.stack[self.sp] = self.stack[self.sp - 1];
+        self.sp += 1;
+    }
+
+    fn drop(&mut self) {
+        self.sp -= 1;
+    }
+
+    fn swap(&mut self) {
+        let tmp = self.stack[self.sp - 1];
+        self.stack[self.sp - 1] = self.stack[self.sp - 2];
+        self.stack[self.sp - 2] = tmp;
+    }
+
+    fn load(&mut self) {
+        let addr = self.stack_pop() as usize;
+        let value = Value::from_code(self.ty_flag, &self.heap[addr..addr + self.ty_flag.size()]);
+        self.value_push(value);
+    }
+
+    fn store(&mut self) {
+        let addr = self.stack_pop() as usize;
+        let value = self.value_pop();
+        let raw = value.to_code();
+        for i in 0..self.ty_flag.size() {
+            self.heap[addr + i] = raw[i];
+        }
+    }
+
+    fn alloc(&mut self) {
+        let size = self.stack_pop() as usize;
+        let addr = self.heap.len();
+        self.heap.resize(addr + size, 0);
+        self.stack_push(addr as u64);
+    }
+
+    fn free(&mut self) {
+        let addr = self.stack_pop() as usize;
+        let size = self.stack_pop() as usize;
+        self.heap.resize(addr + size, 0);
+    }
+
 }
